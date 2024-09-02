ce506be35e1964f1bc18eb96825eb6b34fc20b7e
Faisol <faisol.wr@gmail.com>
2024-09-02 16:59:03 +0700

    [Update] action post

--- BasicDotnetAPI/Controllers/HomeController.cs
+++ BasicDotnetAPI/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace BasicDotnetAPI.Controllers
 {
     [ApiController]
-    public class HomeController : Controller {
+    public class HomeController : Controller
+    {
         [HttpGet]
         [Route("[action]")]
-        public IActionResult Index() {
-            return Ok(new {message = "Hello" });
+        public IActionResult Index()
+        {
+            return Ok(new { message = "Hello" });
+        }
+
+        [HttpPost]
+        [Route("[action]/{name}")]
+        public IActionResult Index(string name)
+        {
+            return Ok(new { message = "Hello " + name });
         }
     }
 
