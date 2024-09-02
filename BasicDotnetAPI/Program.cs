3b560e45211f7f4c9676a72d581242ce6b0185fc
Faisol <faisol.wr@gmail.com>
2024-09-02 18:22:08 +0700

    [Fix] map controllers

--- BasicDotnetAPI/Program.cs
+++ BasicDotnetAPI/Program.cs
@@ -38,6 +38,8 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapControllers();
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
