97a24fbd6085d1ea1c1ca2ee67f8dc29a7cc803e
Faisol <faisol.wr@gmail.com>
2024-09-02 18:33:57 +0700

    [Update] action select from db

--- BasicDotnetAPI/Controllers/HomeController.cs
+++ BasicDotnetAPI/Controllers/HomeController.cs
@@ -51,6 +51,42 @@ namespace BasicDotnetAPI.Controllers
                 });
             }
         }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult List()
+        {
+            try
+            {
+                using NpgsqlConnection conn = new Connect().GetConnection();
+                using NpgsqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM tb_book";
+
+                using NpgsqlDataReader reader = cmd.ExecuteReader();
+                List<object> list = new List<object>();
+
+                while (reader.Read())
+                {
+                    list.Add(new
+                    {
+                        id = Convert.ToInt32(reader["Id"]),
+                        isbn = reader["Isbn"].ToString(),
+                        name = reader["Name"].ToString(),
+                        price = Convert.ToInt32(reader["Price"])
+                    });
+                }
+
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    message = ex.Message
+                });
+            }
+        }
+
     }
 
 }
