45c368f8963c43b1569c79afacafdf412b182ea4
Faisol <faisol.wr@gmail.com>
2024-09-04 11:18:54 +0700

    [Update] mvc api and path

--- BasicDotnetMVC/Controllers/CrudController.cs
+++ BasicDotnetMVC/Controllers/CrudController.cs
@@ -5,13 +5,14 @@ namespace BasicDotnetMVC.Controllers
 {
     public class CrudController : Controller
     {
-        private string Api = "http://localhost:5037/";
+        private string Api = "http://localhost:5037";
         private HttpClient client = new HttpClient();
 
         public async Task<IActionResult> Index()
         {
-            ViewBag.books = await client.GetFromJsonAsync<List<BookModel>>(Api + "List");
+            ViewBag.books = await client.GetFromJsonAsync<List<BookModel>>(Api + "/api/Home/List");
             return View();
         }
+
     }
 }
